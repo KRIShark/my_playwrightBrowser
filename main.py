93e0db42f6e244b3c100a6845a1ea4684210bab5
Kristóf Nagy <17685343+KRIShark@users.noreply.github.com>
2025-11-23 12:03:16 +0100

    example

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def main():
-    print("Hello from my-playwrightbrowser!")
+from playwright.sync_api import sync_playwright
+from playwrightBrowser import NavigateTool, ExtractTextTool
 
-
-if __name__ == "__main__":
-    main()
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    navigate = NavigateTool(sync_browser=browser)
+    navigate.run(url="https://example.com")
+    reader = ExtractTextTool(sync_browser=browser)
+    print(reader.run())
